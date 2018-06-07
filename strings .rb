c00809024dfb7a1778b5cbcabd77b496eb217b12
zariahlee4 <sneakiana@gmail.com>
2018-06-07 16:25:39 +0000

    automatically backed up by learn

--- strings .rb
+++
@@ -1,9 +0,0 @@
-email = "rillertiller@gmail.com"
-address = "123 Nowhere Street, Denver, CO 80132"
-first_name = "rillertiller"
-
-puts "#{first_name} lives at #{address}. Her email address is #{email}."
-
-puts "here is a string"
-puts "1234567"
-puts "**** this is another string ****"
