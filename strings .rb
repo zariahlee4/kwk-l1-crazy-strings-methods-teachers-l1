0c531768474860dd8df9cd0614d6a621ff966643
zariahlee4 <sneakiana@gmail.com>
2018-06-07 16:24:56 +0000

    automatically backed up by learn

--- strings .rb
+++ strings .rb
@@ -1,6 +1,6 @@
 email = "rillertiller@gmail.com"
 address = "123 Nowhere Street, Denver, CO 80132"
-first_name = ""
+first_name = "rillertiller"
 
 puts "#{first_name} lives at #{address}. Her email address is #{email}."
 
